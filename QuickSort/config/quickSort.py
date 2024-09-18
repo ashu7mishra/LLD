336965642230c6212a04fbc2d0795f76ad731199
ashu7mishra <ashu7mishra@gmail.com>
2024-09-18 08:32:08 +0530

    Quick Sort

---
+++ QuickSort/config/quickSort.py
@@ -0,0 +1,11 @@
+import concurrent.futures
+import random
+
+def quicksort(arr):
+    ##Todo
+
+# Example usage:
+arr = random.sample(range(1, 100), 20)
+print("Original array:", arr)
+sorted_arr = quicksort(arr)
+print("Sorted array:", sorted_arr)
