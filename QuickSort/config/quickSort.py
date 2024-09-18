53d5c7011fdb2adb5f0a1c570e49ae62ee036a34
ashu7mishra <ashu7mishra@gmail.com>
2024-09-18 08:39:58 +0530

    Quick Sort

--- QuickSort/config/quickSort.py
+++ QuickSort/config/quickSort.py
@@ -3,7 +3,18 @@ import random
 
 def quicksort(arr):
     ##Todo
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [i for i in arr if i < pivot]
+    middle = [i for i in arr if i == pivot]
+    right = [i for i in arr if i > pivot]
 
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as e:
+        future_left = e.submit(quicksort, left)
+        future_right = e.submit(quicksort, right)
+
+    return future_left.result()+middle+future_right.result()
 # Example usage:
 arr = random.sample(range(1, 100), 20)
 print("Original array:", arr)
