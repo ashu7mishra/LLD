239a98bf876aab28cf813f5f54e4b364fe29a481
ashu7mishra <ashu7mishra@gmail.com>
2025-02-08 18:08:35 +0530

    assignment continue

--- lambda_function/assignments.py
+++ lambda_function/assignments.py
@@ -9,3 +9,28 @@ def getDistinctNumbers(numbers):
 
 print(getDistinctNumbers([1,2,3,2,2,2,3,3,4,4,5,1,2,5,6,7,7,7,8,8,8,9]))
 
+def is_even(num):
+    return num % 2 == 0
+
+
+def double(num):
+    return num * 2
+
+# TODO: Implement the function below
+def filter_and_double_even_numbers(numbers):
+    # code here
+    return list(map(lambda x: double(x),list(filter(lambda x: is_even(x), numbers))))
+
+numbers = [1,2,3,4,5]
+print(filter_and_double_even_numbers(numbers))
+
+# You are given a list of strings having different fruit names. You need to filter out the names of the
+# fruits whose name starts from the character A or a.
+
+fruits = ['apple', 'orange']
+def filter_fruits_starting_with_a(fruits):
+    # Code here
+    return list(filter(lambda x: x[0].upper()=='A', fruits))
+
+print(filter_fruits_starting_with_a(fruits))
+
