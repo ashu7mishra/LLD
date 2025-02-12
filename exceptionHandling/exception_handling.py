21e6fb035ed70365afe974a6b001914eb7b0189f
ashu7mishra <ashu7mishra@gmail.com>
2025-02-12 08:48:17 +0530

    design patterns

--- exceptionHandling/exception_handling.py
+++ exceptionHandling/exception_handling.py
@@ -1,4 +1,26 @@
+# def divide10By(num):
+#     try:
+#         print(10/num)
+#     except ZeroDivisionError:
+#         print("Division by Zero. change your input")
+#         num = int(input())
+#         divide10By(num)
+#     except ValueError:
+#         print("Input should be an integer.")
+#         num = int(input())
+#         divide10By(num)
+#
+#
+#
+# num = int(input())
+# divide10By(num)
+
+class TransactionFailed(Exception):
+    def __init__(self, message):
+        self.message = message
+
 try:
-    print(10/0)
-except ZeroDivisionError:
-    print("Division by Zero")
+    print("Transaction in progress.....")
+    raise TransactionFailed("Failed Transaction")
+except RuntimeError:
+    print("Transaction Failed")
