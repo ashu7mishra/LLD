8763033f060954e5d88c21d76b8f91f0e1be7acb
ashu7mishra <ashu7mishra@gmail.com>
2025-02-13 08:26:37 +0530

    builder design pattern

---
+++ DesignPatterns/BuilderDP/computer.py
@@ -0,0 +1,10 @@
+class Computer:
+
+    def __init__(self):
+        self.ram = None
+        self.cpu = None
+        self.gpu = None
+        self.power_supply = 0
+        self.storage = 0
+
+    
