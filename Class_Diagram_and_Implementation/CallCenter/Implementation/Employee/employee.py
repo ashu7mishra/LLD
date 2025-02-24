92d91c75111475f5142d672c54e07793f7378d65
ashu7mishra <ashu7mishra@gmail.com>
2025-02-24 21:28:06 +0530

    implementation continue

--- Class_Diagram_and_Implementation/CallCenter/Implementation/Employee/employee.py
+++ Class_Diagram_and_Implementation/CallCenter/Implementation/Employee/employee.py
@@ -5,12 +5,12 @@ class Employee(ABC):
 
     def __init__(self, employee_id: str, employee_type: Enum, employee_name: str,
                  employee_gender: str, employee_age: int, call_state: Enum):
-        self.employee_id = employee_id
-        self.employee_type = employee_type
-        self.employee_name = employee_name
-        self.employee_gender = employee_gender
-        self.employee_age = employee_age
-        self.call_state = call_state
+        self._employee_id = employee_id
+        self._employee_type = employee_type
+        self._employee_name = employee_name
+        self._employee_gender = employee_gender
+        self._employee_age = employee_age
+        self._call_state = call_state
 
     @abstractmethod
     def call_handle(self):
@@ -18,4 +18,52 @@ class Employee(ABC):
 
     @abstractmethod
     def escalate_call(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_id(self, employee_id):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_name(self, employee_name):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_age(self, employee_age):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_gender(self, employee_gender):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_type(self, employee_type):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_call_state(self, call_state):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_employee_id(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_employee_name(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_employee_age(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_employee_gender(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_employee_type(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_call_state(self):
         raise NotImplementedError
--- Class_Diagram_and_Implementation/CallCenter/Implementation/Operator/operator.py
+++ Class_Diagram_and_Implementation/CallCenter/Implementation/Operator/operator.py
@@ -1,6 +1,8 @@
 from Class_Diagram_and_Implementation.CallCenter.Implementation.Employee.employee import Employee
 from Class_Diagram_and_Implementation.CallCenter.Implementation.Employee.employee_type import EmployeeType
 from enum import Enum
+from abc import ABC, abstractmethod
+
 
 class Operator(Employee):
 
@@ -8,3 +10,59 @@ class Operator(Employee):
                  employee_gender: str, employee_age: int, call_state: Enum):
         super().__init__(self, employee_id, employee_name, employee_gender, employee_age)
         self._employee_type = EmployeeType.Operator
+
+    @abstractmethod
+    def call_handle(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def escalate_call(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_employee_id(self, employee_id):
+        self._employee_id = employee_id
+
+    @abstractmethod
+    def set_employee_name(self, employee_name):
+        self._employee_name = employee_name
+
+    @abstractmethod
+    def set_employee_age(self, employee_age):
+        self._employee_age = employee_age
+
+    @abstractmethod
+    def set_employee_gender(self, employee_gender):
+        self._employee_gender = employee_gender
+
+    @abstractmethod
+    def set_employee_type(self, employee_type):
+        self._employee_type = employee_type
+
+    @abstractmethod
+    def set_call_state(self, call_state):
+        self._call_state = call_state
+
+    @abstractmethod
+    def get_employee_id(self):
+        return self._employee_id
+
+    @abstractmethod
+    def get_employee_name(self):
+        return self._employee_name
+
+    @abstractmethod
+    def get_employee_age(self):
+        return self._employee_age
+
+    @abstractmethod
+    def get_employee_gender(self):
+        return self._employee_gender
+
+    @abstractmethod
+    def get_employee_type(self):
+        return self._employee_type
+
+    @abstractmethod
+    def get_call_state(self):
+        return self._call_state
